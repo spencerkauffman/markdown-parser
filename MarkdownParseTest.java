a5941eaf620e6077deea00909354a18399d4ea39
spencerkauffman <skauffman@ucsd.edu>
2022-04-21 16:45:07 -0700

    Added test file

---
+++ MarkdownParseTest.java
@@ -0,0 +1,14 @@
+// Imports JUnit assert tests to be used in code
+import static org.junit.Assert.*;
+// Imports all files in JUnit to be used in code
+import org.junit.*;
+// Creates a class called MarkdownParseTest
+public class MarkdownParseTest {
+    // Denotes a test
+    @Test
+    // Test method header
+    public void addition() {
+        // Checks to see if expected value (2) is equal to actual value (1+1)
+        assertEquals(2, 1 + 1);
+    }
+}
